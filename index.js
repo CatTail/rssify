2c0c77f9f7a94b00dd09ca8cb57f7481cdbd8c4b
zhongchiyu <zhongchiyu@gmail.com>
2017-01-14 17:28:48 +0800

    feat: add 404 not found page

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const debug = require('debug')('rssify')
 const handlers = require('moder')(`${__dirname}/handlers`)
 
 const env = process.env.NODE_ENV
-const index = fs.readFileSync('index.html')
+const index = fs.readFileSync('public/index.html')
+const notfound = fs.readFileSync('public/404.html')
 
 const server = micro(async (req, res) => {
   if (req.url === '/') {
@@ -28,13 +29,25 @@ const server = micro(async (req, res) => {
     return
   }
 
-  const feed = await handlers[type](rest)
-  const xml = feed.xml({indent: true})
-  res.writeHead(200, {
-    'Content-Length': Buffer.byteLength(xml),
-    'Content-Type': 'text/xml'
-  })
-  res.end(xml)
+  try {
+    const feed = await handlers[type](rest)
+    const xml = feed.xml({indent: true})
+    res.writeHead(200, {
+      'Content-Length': Buffer.byteLength(xml),
+      'Content-Type': 'text/xml'
+    })
+    res.end(xml)
+  } catch (err) {
+    if (err.statusCode === 404) {
+      res.writeHead(404, {
+        'Content-Length': Buffer.byteLength(notfound),
+        'Content-Type': 'text/html'
+      })
+      res.end(notfound)
+      return
+    }
+    throw err
+  }
 })
 
 server.listen(env === 'production' ? 443 : 3000)
