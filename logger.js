7601adc39f70935c6d1e11a70ecd2bca8fee1b89
Chiyu Zhong <zhongchiyu@gmail.com>
2018-12-29 20:37:57 +0800

    only activate loggy in production

--- logger.js
+++ logger.js
@@ -1,11 +1,13 @@
 const winston = require('winston')
-require('winston-loggly-bulk')
 
-winston.add(winston.transports.Loggly, {
-  token: process.env.RSSIFY_LOGGLY_ACCESS_TOKEN,
-  subdomain: 'cattail',
-  tags: ['rssify'],
-  json: true
-})
+if (process.env.NODE_ENV !== 'development') {
+  require('winston-loggly-bulk')
+  winston.add(winston.transports.Loggly, {
+    token: process.env.RSSIFY_LOGGLY_ACCESS_TOKEN,
+    subdomain: 'cattail',
+    tags: ['rssify'],
+    json: true
+  })
+}
 
 module.exports = winston
